02c18f5ebb2ba578185466e5ba24269e1596101b
vakulabx <103032066+vakulabx@users.noreply.github.com>
2022-04-05 14:58:07 +0530

    Add files via upload
    PI Process Automation phase1 code.

---
+++ brd_pkg_merge.tcl
@@ -0,0 +1,7 @@
+sigrity::open document {E:/Share/VakulabharanamX/pkg_and_brd_files/pkg_file/dg256eu_ww45p3_pisimulation.tgz} {!}
+sigrity::import stackup {C:\Users\vakulabx\Desktop\StackUp.csv} {!}
+sigrity::save {!} 
+sigrity::close document {!} 
+sigrity::open document {E:/Share/VakulabharanamX/pkg_and_brd_files/brd_file/M52606-001_brd/M52606-001.brd} {!}
+sigrity::import PKG -SPDFile {E:/Share/VakulabharanamX/pkg_and_brd_files/pkg_file/dg256eu_ww45p3_pisimulation.spd} -OldCkt {BRDU1} -NewCkt {PKGA1} -method {SolderBall} -MatchSel {InheritPkg} -unit {mm} -height {4.7264e-02} -radius {2.4498e-02} -Prefix -ApplyTo {PKG&PCB} {!}
+sigrity::save {!} 
