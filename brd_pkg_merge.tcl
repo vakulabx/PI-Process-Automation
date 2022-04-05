ae236a66983e0fd0b78d87b931c709643aa1d870
vakulabx <103032066+vakulabx@users.noreply.github.com>
2022-04-05 18:11:21 +0530

    Update brd_pkg_merge.tcl
    sigrity venkatesh {!} added

--- brd_pkg_merge.tcl
+++ brd_pkg_merge.tcl
@@ -5,3 +5,4 @@ sigrity::close document {!}
 sigrity::open document {E:/Share/VakulabharanamX/pkg_and_brd_files/brd_file/M52606-001_brd/M52606-001.brd} {!}
 sigrity::import PKG -SPDFile {E:/Share/VakulabharanamX/pkg_and_brd_files/pkg_file/dg256eu_ww45p3_pisimulation.spd} -OldCkt {BRDU1} -NewCkt {PKGA1} -method {SolderBall} -MatchSel {InheritPkg} -unit {mm} -height {4.7264e-02} -radius {2.4498e-02} -Prefix -ApplyTo {PKG&PCB} {!}
 Sigrity::save {!}
+Sigrity::Venkatesh {!}
